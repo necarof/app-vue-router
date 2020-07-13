8428038469f05220a847bd06525441d74aa07ba0
Nikita <43652463+necarof@users.noreply.github.com>
2020-07-13 10:57:43 +0600

    add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="container">
-    <router-view />
-  </div>
+  <div class="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/cars">Cars</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view />
+    </div>
+  </div>  
 </template>
 
 <script>
